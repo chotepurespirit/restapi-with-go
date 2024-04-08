5f9a82cff03dc228dc74babd8193916cfc467258
chotepurespirit <chovyzzz.nai@gmail.com>
2024-04-09 00:02:17 +0700

    create req.method and log.Println

--- gohttp/simple_http.go
+++ gohttp/simple_http.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	"io/ioutil"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 type User struct {
-	ID int `json:"id"` //สามารถใช้ json name เพื่อเปลี่ยนชื่อใน json ได้
+	ID   int    `json:"id"` //สามารถใช้ json name เพื่อเปลี่ยนชื่อใน json ได้
 	Name string `json:"name"`
-	Age int `json:"age"`
+	Age  int    `json:"age"`
 }
 
 var users = []User{
@@ -21,6 +22,7 @@ var users = []User{
 func main() {
 	http.HandleFunc("/users", func(w http.ResponseWriter, req *http.Request) {
 		if req.Method == "GET" {
+			log.Println("GET")
 			//เราไม่สามารถที่จะใส่ w.Write(users) ตรงๆ ได้เพราะมันเป็น type slice ไม่สามารถแปลงเป็น byte ได้
 			//ดังนั้นเราจะแปลง struct ให้เป็น json ก่อน
 			//โดยเราจะต้อง import package encoding/json มาใช้งาน
@@ -36,23 +38,30 @@ func main() {
 			return
 		}
 		if req.Method == "POST" {
-			body, err := ioutil.ReadAll(req.Body) //ReadAll จะทำการอ่าน body ทั้งหมด
+			log.Println("POST")
+			body, err := ioutil.ReadAll(req.Body) //ใช้ package ioutil ในการอ่าน body จาก request
 			// ต่อจากนั้นให้ทำการ handle error ก่อน
 			if err != nil {
-	
+				w.WriteHeader(http.StatusInternalServerError) //ถ้ามี error ให้ return 500
+				w.Write([]byte(err.Error()))                  //แสดง error ออกมา
+				return
 			}
-		}
+			// จากนั้นให้ทำการแปลง body ที่ได้มาจาก request ให้เป็น struct
+			var u User                     //สร้างตัวแปร u ขึ้นมาเพื่อเก็บค่าที่ได้จากการแปลง
+			err = json.Unmarshal(body, &u) //ตรงนี้จะต้องส่ง pointer ไปเพื่อให้ json.Unmarshal แก้ไขค่าใน struct ได้
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest) //ถ้ามี error ให้ return 400
+				w.Write([]byte(err.Error()))         //แสดง error ออกมา
+				return
+			}
 
-		})
-		/*check method
-		if req.Method == "POST" {
-			w.Write([]byte(`{"name" : "Chozzze" , "method" : "POST"}`))
+			users = append(users, u) //เพิ่มข้อมูลใหม่เข้าไปใน slice
+
+			fmt.Fprintf(w, "hello %s created users", "POST") //แสดงข้อความออกไป
 			return
 		}
 
-		//writeHEADER
-		w.WriteHeader(http.StatusMethodNotAllowed)
-	})*/
+	})
 	log.Println("Starting server on :2565")
 	log.Fatal(http.ListenAndServe(":2565", nil)) //คำสั่ง ListenAndServe จะทำงานแบบค้างเอาไว้ จนกว่าจะมีการปิด server
 	log.Println("bye bye")
